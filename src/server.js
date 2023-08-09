20f4c4493ddfcd37f5a7ca0fe5b34d52706c3aa6
bion0625 <stxtory@gmail.com>
2023-08-09 09:10:35 +0900

    express init

---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import morgan from "morgan";
+import session from "express-session";
+import flash from "express-flash";
+import MongoStore from "connect-mongo";
+import rootRouter from "./router/rootRouter";
+import userRouter from "./router/userRouter";
+import videoRouter from "./router/videoRouter";
+import { localsMiddleware } from "./middleware";
+import apiRouter from "./router/apiRouter";
+
+const app = express();
+const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+
+app.use(logger);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(session({
+    secret:process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl:process.env.DB_URL})
+}))
+
+app.use(flash());
+app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
+app.use("/staticfile", express.static("staticfile"));
+app.use("/static", express.static("assets"));
+app.use("/", rootRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+app.use("/api", apiRouter);
+
+export default app;
